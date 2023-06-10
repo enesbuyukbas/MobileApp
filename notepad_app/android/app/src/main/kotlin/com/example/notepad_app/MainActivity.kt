255b692a628ff7a35d73ab075759d3f80053f508
Enes Büyükbaş <enesbykbs20@gmail.com>
2023-06-11 00:29:07 +0300

    düzenlendi

--- notepad_app/android/app/src/main/kotlin/com/example/notepad_app/MainActivity.kt
+++ notepad_app/android/app/src/main/kotlin/com/example/notepad_app/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.notepad_app
 
 import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 }
---
+++ notepad_app/android/app/src/main/kotlin/com/example/notepad_app/MyApplication.kt
@@ -0,0 +1,9 @@
+package com.example.notepad_app
+
+import io.flutter.app.FlutterApplication
+
+class MyApplication : FlutterApplication() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
